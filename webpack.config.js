90b994d986a3ea33cf6c56c451cd63ea836c0d87
webel3 <webel3@bfh.ch>
2016-11-18 15:17:17 +0100

    initial project setup:  vue.js (mvc js framework), bootstrap (css framework), vue-resource (vue plugin providing services for http requests) and webpack (module bundler).

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var webpack = require("webpack");
+var path = require('path');
+
+// @see: https://webpack.github.io/docs/configuration.html
+module.exports = {
+
+  // the entry point for the bundle
+  entry: './app.js',
+
+  // options for the output file of the bundling process.
+  output: {
+    path: __dirname + '/target',
+    filename: 'bundle.js'
+  },
+
+  // array of extensions that should be used to resolve modules
+  resolve: {
+    modules: [
+      'node_modules', path.resolve(__dirname, 'src')
+    ],
+    extensions: ['.ts', '.js'],
+    alias: {
+      'vue$': 'vue/dist/vue.js'
+    }
+  },
+
+  // a source-map is emitted
+  devtool: 'source-map',
+
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        use: ['awesome-typescript']
+      },
+      {
+        test: /\.html$/,
+        use: 'raw-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  ],
+
+  // @see: https://webpack.github.io/docs/webpack-dev-server.html
+  devServer: {
+    inline: true,
+    hot: true,
+    proxy: {
+      '/rest': {
+        target: 'url-to-dreamfactory-rest-services',
+        secure: false
+      }
+    }
+  }
+};
