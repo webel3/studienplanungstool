bd462a42f52849db3730b8926e02bd0a0dbc47d0
webel3 <webel3@bfh.ch>
2017-01-02 15:39:10 +0100

    Helper objects created // some mocks removed // code cleanup

--- src/components/dynamic-table/dynamic-table.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-
-let DynamicTable = Vue.component('dynamic-table', {
-    props: ['headers', 'rows'],
-    template: require('./dynamic-table.html')
-});
-
-export default DynamicTable;
