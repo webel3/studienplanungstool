df178b5c7cc09984324c80f7c8b5cc42e427784a
engek1 <engek1@bfh.ch>
2017-01-05 11:06:14 +0100

    add bookings and remove bookings on modules page

--- src/pages/modules/modules.js
+++ src/pages/modules/modules.js
@@ -9,212 +9,235 @@ import SemesterHelper from '../../helpers/SemesterHelper';
 import HttpConfig from '../../rest/HttpConfig';
 import Endpoints from '../../rest/Endpoints';
 
+function getUserId() {
+  return JSON.parse(sessionStorage.getItem('user')).uid;
+}
+
 let Modules = {
-    template: require('./modules.html'),
+  template: require('./modules.html'),
 
-    data: function() {
-        return {
-            startingSemester: 201201, // TODO
-            upcomingSemester: JSON.parse(sessionStorage.getItem('user')).upcomingsemester,
-            executions: [],
-            bookings: [],
-            searches: {
-                module: '',
-                places: {
-                    model: [],
-                    options: [{
-                        value: 'BER',
-                        name: 'Bern'
-                    }, {
-                        value: 'BIE',
-                        name: 'Biel'
-                    }]
-                },
-                types: {
-                    model: [],
-                    options: [{
-                        value: 'PM',
-                        name: 'Pflicht'
-                    }, {
-                        value: 'WM',
-                        name: 'Wahlpflicht'
-                    }, {
-                        value: 'WW',
-                        name: 'Wahl'
-                    }]
-                },
-                groups: {
-                    model: [],
-                    options: [{
-                        value: 'A',
-                        name: 'A'
-                    }, {
-                        value: 'B',
-                        name: 'B'
-                    }, {
-                        value: 'C',
-                        name: 'C'
-                    }, {
-                        value: 'D',
-                        name: 'D'
-                    }, {
-                        value: 'D1',
-                        name: 'D1'
-                    }, {
-                        value: 'D2',
-                        name: 'D2'
-                    }]
-                }
-            }
+  data: function () {
+    return {
+      upcomingSemester: JSON.parse(sessionStorage.getItem('user')).upcomingsemester,
+      executions: [],
+      bookings: [],
+      searches: {
+        module: '',
+        places: {
+          model: [],
+          options: [{
+            value: 'BER',
+            name: 'Bern'
+          }, {
+            value: 'BIE',
+            name: 'Biel'
+          }]
+        },
+        types: {
+          model: [],
+          options: [{
+            value: 'PM',
+            name: 'Pflicht'
+          }, {
+            value: 'WM',
+            name: 'Wahlpflicht'
+          }, {
+            value: 'WW',
+            name: 'Wahl'
+          }]
+        },
+        groups: {
+          model: [],
+          options: [{
+            value: 'A',
+            name: 'A'
+          }, {
+            value: 'B',
+            name: 'B'
+          }, {
+            value: 'C',
+            name: 'C'
+          }, {
+            value: 'D',
+            name: 'D'
+          }, {
+            value: 'D1',
+            name: 'D1'
+          }, {
+            value: 'D2',
+            name: 'D2'
+          }]
         }
-    },
+      }
+    }
+  },
 
-    mounted: function() {
-        this.$nextTick(function() { // ensure that DOM element exists prior to use jquery
-            jQuery('.selectpicker').selectpicker();
+  mounted: function () {
+    this.$nextTick(function () { // ensure that DOM element exists prior to use jquery
+      jQuery('.selectpicker').selectpicker();
 
-            /* unfortunately, we have to use that bugfix to get it working.
-             * otherwise, the 'open' class won't get toggled and the dropdown is never shown.
-             */
-            jQuery(".bootstrap-select").click(function () {
-                jQuery(this).toggleClass('open');
-            });
+      /* unfortunately, we have to use that bugfix to get it working.
+       * otherwise, the 'open' class won't get toggled and the dropdown is never shown.
+       */
+      jQuery(".bootstrap-select").click(function () {
+        jQuery(this).toggleClass('open');
+      });
+    });
+  },
+
+  created: function () {
+    let _self = this;
+    let queryString = ['?filter=(student_id="', getUserId(), '")'].join('');
+
+    Promise.all([
+      this.$http.get(Endpoints.COURSE_EXECUTION_VIEW, HttpConfig),
+      this.$http.get(Endpoints.EXECUTION_SLOT, HttpConfig),
+      this.$http.get(Endpoints.STUDENT_COURSE_EXECUTION + queryString, HttpConfig) // Bookings
+    ]).then(function (responses) {
+      _self.executions = responses[0].body.resource;
+
+      _self.executions.forEach(exec => {
+        let option = _self.searches.places.options.filter(option => {
+          return option.value === exec.place;
         });
-    },
+        exec.place = option[0].name;
+        exec.semester = SemesterHelper.split(SemesterHelper.NOW_REFERENCE);
+      });
 
-    created: function() {
-        let _self = this;
-
-        Promise.all([
-            this.$http.get(Endpoints.COURSE_EXECUTION_VIEW, HttpConfig),
-            this.$http.get(Endpoints.EXECUTION_SLOT, HttpConfig)
-        ]).then(function (responses) {
-            _self.executions = responses[0].body.resource;
-
-            _self.executions.forEach(exec => {
-                let option = _self.searches.places.options.filter(option => {
-                    return option.value === exec.place;
-                });
-                exec.place = option[0].name;
-                exec.semester = SemesterHelper.add(_self.startingSemester, _self.upcomingSemester);
-            });
-
-            // TODO: bookings auch noch laden (FK's reichen)
-            // TODO: jene Module aus executions rauslöschen, die bereits gebucht sind
-
-            responses[1].body.resource.forEach(slot => {
-                _self.executions.forEach(exec => {
-                    if (exec.uid === slot.courseexecution_id) {
-                        if (!exec.slots) {
-                            exec.slots = [];
-                        }
-                        slot.day = DayOfWeek.getDay(slot.dayofweek);
-                        slot.start = slot.start.substr(0, 5);
-                        slot.end = slot.end.substr(0, 5);
-                        exec.slots.push(slot);
-                    }
-                });
-            });
-        }, responses => {
-            console.log("something went wrong:", responses);
-        });
-    },
-
-    methods: {
-        add: function(execution) {
-            let item = null;
-            this.executions.forEach(elem => {
-                if (elem.executioncode === execution.executioncode) { // i.e. "BTI7021b"
-                    item = elem;
-                }
-            });
-
-            // TODO: erst machen, wenn der POST Request zum Backend erfolgreich war
-            this.executions.splice(this.executions.indexOf(item), 1);
-            this.bookings.push(item);
-        },
-
-        remove: function(execution) {
-            // TODO: zuerst POST Request, erst danach die model changes
-
-            let item = this.bookings.splice(this.bookings.indexOf(execution), 1);
-            this.executions.push(item[0]);
-        },
-
-        formatSemester: function(incremental) {
-            let increment = this.upcomingSemester;
-            if (incremental > 0) {
-                increment += incremental;
+      // assign time slots to executions
+      responses[1].body.resource.forEach(slot => {
+        _self.executions.forEach(exec => {
+          if (exec.uid === slot.courseexecution_id) {
+            if (!exec.slots) {
+              exec.slots = [];
             }
-            let info = SemesterHelper.add(this.startingSemester, increment);
-            return [info.type.name, info.year].join(' ');
+            slot.day = DayOfWeek.getDay(slot.dayofweek);
+            slot.start = slot.start.substr(0, 5);
+            slot.end = slot.end.substr(0, 5);
+            exec.slots.push(slot);
+          }
+        });
+      });
+
+      // remove bookings from executions, add them to bookings
+      responses[2].body.resource.forEach(booking => {
+        _self.executions.forEach(execution => {
+          if (execution.uid === booking.courseexecution_ID) {
+            _self.bookings.push(execution);
+            _self.executions.splice(_self.executions.indexOf(execution), 1);
+          }
+        });
+      });
+
+    }, responses => {
+      console.log("something went wrong:", responses);
+    });
+
+  },
+
+  methods: {
+
+    createRequestBody: function (execution) {
+      return {
+        "resource": {
+          "student_ID": getUserId(),
+          "courseexecution_ID": execution.uid
         }
+      };
     },
 
-    computed: {
-        filteredExecutions: function() {
-            return this.executions
+    add: function (execution) {
+      this.$http.post(Endpoints.STUDENT_COURSE_EXECUTION, this.createRequestBody(execution), HttpConfig).then((response) => {
+        this.executions.splice(this.executions.indexOf(execution), 1);
+        this.bookings.push(execution);
+      }, (response) => {
+        window.console.error(response);
+      });
 
-                // check if the module name matches the searched input string.
-                .filter(item => {
-                    if (item.course_name_de.toLowerCase().indexOf(this.searches.module.trim().toLowerCase()) > -1) {
-                        return item;
-                    }
-                })
-
-                // if there is at least one place selected, check for it.
-                .filter(item => {
-                    if (this.searches.places.model.length > 0) {
-                        if (this.searches.places.model.indexOf(item.place) > -1) {
-                            return item;
-                        }
-                    } else return item;
-                })
-
-                // if there is at least one module type selected, check for it.
-                .filter(item => {
-                    if (this.searches.types.model.length > 0) {
-                        if (this.searches.types.model.indexOf(item.coursetype) > -1) {
-                            return item;
-                        }
-                    } else return item;
-                })
-
-                // if there is at least one module group selected, check for it.
-                .filter(item => {
-                    if (this.searches.groups.model.length > 0) {
-                        if (this.searches.groups.model.indexOf(item.coursegroup) > -1) {
-                            return item;
-                        }
-                    } else return item;
-                })
-            ;
-        },
-
-        filteredBookings: function() {
-            let info = SemesterHelper.add(this.startingSemester, this.upcomingSemester);
-            let label = parseInt([info.year, info.type.value].join(''));
-
-            return this.bookings.filter(elem => {
-                if (elem.semester.year === info.year && elem.semester.type === info.type) {
-                    return elem;
-                }
-            });
-        },
-
-        filteredBookingsAfterNext: function() {
-            let info = SemesterHelper.add(this.startingSemester, this.upcomingSemester + 1);
-            let label = parseInt([info.year, info.type.value].join(''));
-
-            return this.bookings.filter(elem => {
-                if (elem.semester === label) {
-                    return elem;
-                }
-            });
-        }
     },
 
-    components: { ExecutionTile, jQuery, BootstrapSelect }
+    remove: function (execution) {
+      let options = Object.assign({}, HttpConfig);
+      options.body = this.createRequestBody(execution);
+      this.$http.delete(Endpoints.STUDENT_COURSE_EXECUTION, options).then((response) => {
+        this.bookings.splice(this.bookings.indexOf(execution), 1);
+        this.executions.push(execution);
+      }, (response) => {
+        window.console.error(response);
+      });
+    },
+
+    formatSemester: function (incremental) {
+      let increment = incremental || 0;
+      let info = SemesterHelper.add(SemesterHelper.NOW_REFERENCE, increment);
+      return [info.type.name, info.year].join(' ');
+    }
+  },
+
+  computed: {
+    filteredExecutions: function () {
+      return this.executions
+
+      // check if the module name matches the searched input string.
+      .filter(item => {
+        if (item.course_name_de.toLowerCase().indexOf(this.searches.module.trim().toLowerCase()) > -1) {
+          return item;
+        }
+      })
+
+      // if there is at least one place selected, check for it.
+      .filter(item => {
+        if (this.searches.places.model.length > 0) {
+          if (this.searches.places.model.indexOf(item.place) > -1) {
+            return item;
+          }
+        } else return item;
+      })
+
+      // if there is at least one module type selected, check for it.
+      .filter(item => {
+        if (this.searches.types.model.length > 0) {
+          if (this.searches.types.model.indexOf(item.coursetype) > -1) {
+            return item;
+          }
+        } else return item;
+      })
+
+      // if there is at least one module group selected, check for it.
+      .filter(item => {
+        if (this.searches.groups.model.length > 0) {
+          if (this.searches.groups.model.indexOf(item.coursegroup) > -1) {
+            return item;
+          }
+        } else return item;
+      })
+        ;
+    },
+
+    filteredBookings: function () {
+      let info = SemesterHelper.split(SemesterHelper.NOW_REFERENCE);
+      let label = parseInt([info.year, info.type.value].join(''));
+
+      return this.bookings.filter(elem => {
+        if (elem.semester.year === info.year && elem.semester.type === info.type) {
+          return elem;
+        }
+      });
+    },
+
+    filteredBookingsAfterNext: function () {
+      let info = SemesterHelper.add(SemesterHelper.NOW_REFERENCE, 1);
+      let label = parseInt([info.year, info.type.value].join(''));
+
+      return this.bookings.filter(elem => {
+        if (elem.semester === label) {
+          return elem;
+        }
+      });
+    }
+  },
+
+  components: {ExecutionTile, jQuery, BootstrapSelect}
 };
 
 export default Modules;
